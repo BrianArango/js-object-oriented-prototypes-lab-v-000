caa933520144264649f011241924e196892636a6
Brian Arango <brianvictus@gmail.com>
2019-07-23 00:59:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,11 @@
      return "I like to help people.";
    };
 
-   BoardMemeber.prototype.releasePressStatement = function(){
+   BoardMember.prototype.releasePressStatement = function(){
      return "You will see great things from Scuber.";
    };
 
-   BoardMemeber.prototype.sayHi = function(){
+   BoardMember.prototype.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
    };
 
